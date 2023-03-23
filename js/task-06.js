8ec282854be992fdb3d2c8795d34184617bade39
TKazhukalo <118135022+TKazhukalo@users.noreply.github.com>
2023-03-23 21:03:36 +0200

    update-task-06

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,16 @@
 const inputBlurEl = document.querySelector('#validation-input');
+
 inputBlurEl.addEventListener('blur', onInputBlur);
 
 
 function onInputBlur() {
     
     if (inputBlurEl.getAttribute('data-length') <= inputBlurEl.value.length) {
-     //console.log(inputBlurEl.value.length);
+        //console.log(inputBlurEl.value.length);
         inputBlurEl.classList.add('valid');
-    }
-    inputBlurEl.classList.add('invalid');
- console.log(`Input потерял фокус -событие блюр`);
-}
+        console.log(inputBlurEl.getAttribute('.data-length'));
+    } else {
+        inputBlurEl.classList.add('invalid');
+        console.log(`Input потерял фокус -событие блюр`);
+    };
+};
