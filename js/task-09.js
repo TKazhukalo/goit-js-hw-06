eba9dca64669037b98f19b4714e0fd38fce32531
TKazhukalo <118135022+TKazhukalo@users.noreply.github.com>
2023-03-24 15:33:03 +0200

    update-09-task

--- js/task-09.js
+++ js/task-09.js
@@ -3,12 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const bodyEl = document.body;
 const widgetEl = document.querySelector('.widget');
 const colorEl = document.querySelector('.color');
 const btnChangeEl = document.querySelector('.change-color');
 btnChangeEl.addEventListener('click', onClick);
 function onClick() {
-  widgetEl.style.background = getRandomHexColor();
+  bodyEl.style.background = getRandomHexColor();
  // console.log(getRandomHexColor());
  colorEl.textContent = `${getRandomHexColor()}`;
  // console.log(event.currentTarget);
